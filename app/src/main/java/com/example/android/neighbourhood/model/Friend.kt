e0abd0dc09c009466efa8b0bffc8dfe62d672310
Yash Parmar <yashparmar009@outlook.com>
2022-12-09 13:04:35 +0530

    Code reformatted!

--- app/src/main/java/com/example/android/neighbourhood/model/Friend.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.neighbourhood.model
-
-data class Friend(
-    val name: String = "",
-    val photoUrl: String? = "",
-    val userEmail: String? = ""
-)
--- app/src/main/java/com/example/android/neighbourhood/model/FriendlyMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.neighbourhood.model
-
-data class FriendlyMessage(
-    val text: String? = null,
-    val name: String? = null,
-    val email: String? = null,
-    val photoUrl: String? = null,
-    val imageUrl: String? = null,
-)
